37e9664575fe222d347ca6b7564e0063fadb2b88
melnelen <melnelen@gmail.com>
2020-03-22 21:57:34 +0100

    Initial Commit

---
+++ Shamaz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Shamaz
+//
+//  Created by Alexandra Ivanova on 22/03/2020.
+//  Copyright © 2020 Alex Ivanova. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
