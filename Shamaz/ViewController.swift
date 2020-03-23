c0bd01e524182fdcf81d2d099f83164faa5a0c43
melnelen <melnelen@gmail.com>
2020-03-23 20:47:59 +0100

    Implement the initial screen UI and connect UI elements to code.

--- Shamaz/ViewController.swift
+++ Shamaz/ViewController.swift
@@ -10,11 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var storytellingTask: UILabel!
+    
+    @IBAction func genereteFutureStorytellingTask(_ sender: Any) {
     }
-
-
+    
+    @IBAction func generetePastStorytellingTask(_ sender: Any) {
+    }
+    
+    
 }
 
